cf3dc72347a74551bac675124b5a3a31554ca6f5
om-varshney <63551805+om-varshney@users.noreply.github.com>
2023-05-01 11:01:40 +0530

    Partial Cosmetic Enhancements added.

--- src/Helpers/Menus/Menu.java
+++ src/Helpers/Menus/Menu.java
@@ -1,9 +1,11 @@
 package Helpers.Menus;
 
+import Helpers.PrettyPrint;
 import de.vandermeer.asciitable.AsciiTable;
 import de.vandermeer.asciitable.CWC_LongestLine;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public abstract class Menu {
@@ -21,11 +23,19 @@ public abstract class Menu {
         Scanner sc = new Scanner(System.in);
         while (true) {
             System.out.print("Enter your Choice: ");
-            int choice = sc.nextInt();
+            int choice;
+            try {
+                choice = sc.nextInt();
+            } catch (InputMismatchException im) {
+                System.out.println(PrettyPrint.ANSI_RED_BACKGROUND + "Enter valid Choice!" + PrettyPrint.ANSI_RESET);
+                // Consume invalid input from the scanner buffer
+                sc.nextLine();
+                continue;
+            }
             if (choice > 0 && choice <= this.getOptions().size()) {
                 return choice;
             } else {
-                System.out.println("Enter valid Choice!");
+                System.out.println(PrettyPrint.ANSI_RED_BACKGROUND + "Enter valid Choice!" + PrettyPrint.ANSI_RESET);
             }
         }
     }
